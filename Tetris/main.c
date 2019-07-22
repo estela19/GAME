09264cccf541c1b3de805e84a03e9adc880ed691
estela19 <e9857256@naver.com>
2019-07-22 17:33:12 +0900

    beta version
    add level & clear check, update check logic & rotate logic

--- Tetris/main.c
+++ Tetris/main.c
@@ -27,10 +27,10 @@ void set_tetrimino_value(block* p_tetrimino);
 void make_new_tetrimino(block* p_tetrimino);
 void print_tetrimino(block* p_tetrimino);
 void previous_tetrimino_print(void);
-void check(block* p_tetrimino, int x, int y);
+int check(block* p_tetrimino, int rotate, int x, int y);
 void delete_tetrimino_board(block* p_tetrimino);
 void add_tetrimino_board(block* p_tetrimino);
-int next_stage_check(block* p_tetrimino);
+int next_stage_check(block* p_tetrimino, int* score);
 
 /*
 0x20 스페이스
@@ -42,185 +42,188 @@ int next_stage_check(block* p_tetrimino);
 
 const int tetriminos[7][4][5][5] =
 { {   {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   }   }   },
-
-   {   {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   1,   1,   1,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
-
-	 {   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
-
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   1,   1,   1,   1,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
-
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   }   }   },
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   }   }   },
 
    {   {   {   0,   0,   0,   0,   0,   },
-	   {   0,   1,   0,   0,   0,   },
-	   {   0,   1,   1,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   1,   1,   1,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	{   {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   1,   1,   1,   0,   },
-	   {   0,   0,   0,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   1,   1,   1,   1,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   1,   1,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   }   },
-
-   {   {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   1,   1,   1,   0,   },
-	   {   0,   1,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
-
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   1,   1,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
-
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   1,   0,   },
-	   {   0,   1,   1,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
-
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   }   }   },
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   }   }   },
 
    {   {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   1,   1,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	  {   0,   1,   0,   0,   0,   },
+	  {   0,   1,   1,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   0,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   1,   1,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   1,   1,   1,   0,   },
+	  {   0,   0,   0,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   0,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   }   }   },
-
-   {   {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   1,   1,   0,   0,   },
-	   {   0,   1,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
-
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   1,   1,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
-
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   1,   1,   0,   0,   },
-	   {   0,   1,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
-
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   1,   1,   0,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   }   },
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   1,   1,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   }   },
 
    {   {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   1,   1,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   1,   1,   1,   0,   },
+	  {   0,   1,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   1,   0,   0,   0,   },
-	   {   0,   1,   1,   0,   0,   },
-	   {   0,   1,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   1,   1,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   1,   1,   1,   0,   },
-	   {   0,   0,   1,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   0,   0,   }   },
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   1,   0,   },
+	  {   0,   1,   1,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
 
-	 {   {   0,   0,   0,   0,   0,   },
-	   {   0,   0,   0,   1,   0,   },
-	   {   0,   0,   1,   1,   0,   },
-	   {   0,   0,   0,   1,   0,   },
-	   {   0,   0,   0,   0,   0,   }   }   } };
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   }   }   },
+
+   {   {   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   1,   1,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
+
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   0,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
+
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   1,   1,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
+
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   0,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   }   }   },
+
+   {   {   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   1,   1,   0,   0,   },
+	  {   0,   1,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
+
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   1,   1,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
+
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   1,   1,   0,   0,   },
+	  {   0,   1,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
+
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   1,   1,   0,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   }   },
+
+   {   {   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   1,   1,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
+
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   1,   0,   0,   0,   },
+	  {   0,   1,   1,   0,   0,   },
+	  {   0,   1,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
+
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   1,   1,   1,   0,   },
+	  {   0,   0,   1,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   0,   0,   }   },
+
+	{   {   0,   0,   0,   0,   0,   },
+	  {   0,   0,   0,   1,   0,   },
+	  {   0,   0,   1,   1,   0,   },
+	  {   0,   0,   0,   1,   0,   },
+	  {   0,   0,   0,   0,   0,   }   }   } };
 
 const char blank = { ' ' };
 int board[22][21] = { 0, };
-int visit[22][21] = { 0, };
 
 int main()
 {
+	srand(time(NULL));
 	block tetrimino;
 	block* p_tetrimino = &tetrimino;
 	int line_cnt = 0;
 	int immeadiatly_flag = 0;
 	int fast_flag = 0;
+	int score = 0;
+	int level = 1;
 	double time_interval_update = 0.6;
+	double time_interval = 0.6;
 	double time_start;
 	double time_now;
 
@@ -246,19 +249,10 @@ int main()
 
 
 	int  j;
-	/*
-	 for (i = 0; i < 22; i++) {
-	   for (j = 0; j < 21; j++) {
-		  Move_Cursor(i, j);
-		  printf("%d ", board[i][j]);
-	   }
-	   printf("\n");
-	 }
 
-   //   */
 	while (1)
 	{
-		time_interval_update = 0.6;
+		int go_down = 1;
 		fast_flag = 0;
 		immeadiatly_flag = 0;
 		Accept_Inputs();
@@ -266,10 +260,7 @@ int main()
 		if (p_tetrimino->near_block[1] == 1)
 			immeadiatly_flag = 0;
 
-		memset(visit, 0, sizeof(visit));
-		check(p_tetrimino, tetrimino.pos.x + 2, tetrimino.pos.y + 2);
-
-		//		delete_tetrimino_board(p_tetrimino);
+		time_interval_update = time_interval;
 		print_blank(p_tetrimino);
 
 		p_tetrimino->pre_pos.x = p_tetrimino->pos.x;
@@ -288,13 +279,16 @@ int main()
 			{
 				//스페이스바
 				if (info.type == TypeCode_Key_Press && info.code == 0x20) {
-					immeadiatly_flag = 1;
+					delete_tetrimino_board(p_tetrimino);
+					while ((go_down = check(p_tetrimino, p_tetrimino->rotate, p_tetrimino->pos.x, p_tetrimino->pos.y + 1)) != 0) {
+						p_tetrimino->pos.y += 1;
+					}
 				}
 
 				//위쪽키
 				if (info.type == TypeCode_Key_Press && info.code == 0x26) {
-					// TODO: 회전 못하는 경우도 고려.
-					rotate_tetrimino(p_tetrimino);
+					if (check(p_tetrimino, p_tetrimino->rotate + 1, p_tetrimino->rotate, p_tetrimino->pos.x) != 0)
+						rotate_tetrimino(p_tetrimino);
 				}
 
 				//아래키
@@ -304,13 +298,13 @@ int main()
 
 				//왼쪽키
 				if (info.type == TypeCode_Key_Press && info.code == 0x25) {
-					if (p_tetrimino->near_block[2] == 0)
+					if (check(p_tetrimino, p_tetrimino->rotate, p_tetrimino->pos.x - 1, p_tetrimino->pos.y) != 0)
 						p_tetrimino->pos.x -= 1;
 				}
 
 				//오른쪽키
 				if (info.type == TypeCode_Key_Press && info.code == 0x27) {
-					if (p_tetrimino->near_block[0] == 0)
+					if (check(p_tetrimino, p_tetrimino->rotate, p_tetrimino->pos.x + 1, p_tetrimino->pos.y) != 0)
 						p_tetrimino->pos.x += 1;
 				}
 			}
@@ -321,16 +315,10 @@ int main()
 				pos = Get_Mouse_Pos();
 
 			}
-			memset(visit, 0, sizeof(visit));
-			check(p_tetrimino, tetrimino.pos.x + 2, tetrimino.pos.y + 2);
 		}
 
-		//		add_tetrimino_board(p_tetrimino);
 
-
-
-
-				//시간에따라 블록이 내려옴
+		//시간에따라 블록이 내려옴
 		if (fast_flag == 1)
 			time_interval_update = 0.1;
 
@@ -338,35 +326,37 @@ int main()
 			time_interval_update = 0.01;
 
 		if (Stopwatch_Get() - time_now > time_interval_update) {
-			//			if (p_tetrimino->near_block[1] == 0)
-			p_tetrimino->pos.y += 1;
+			if ((go_down = check(p_tetrimino, p_tetrimino->rotate, p_tetrimino->pos.x, p_tetrimino->pos.y + 1)) != 0)
+				p_tetrimino->pos.y += 1;
 			time_now = Stopwatch_Get();
 
-
-
-			for (i = 0; i < 22; i++) {
-				for (j = 0; j < 21; j++) {
-					Move_Cursor(2 * i + 21, j);
-					printf("%d ", board[i][j]);
-				}
-			}
-			// */
-			Move_Cursor(30, 25);
-			//printf("왼쪽 %d, 오른쪽 %d, ", p_tetrimino->near_block[2], p_tetrimino->near_block[0]);
-			//printf("아래쪽 %d, 위쪽 %d", p_tetrimino->near_block[1], p_tetrimino->near_block[3]);
 		}
 
 		//블록이 바닥에 놓이면 새로운 블럭 생성
-		if (p_tetrimino->near_block[1] == 1)
+		if (go_down == 0) {
 			make_new_tetrimino(p_tetrimino);
+			time_interval_update = time_interval;
+		}
 
 		delete_tetrimino_board(p_tetrimino);
 		add_tetrimino_board(p_tetrimino);
-		line_cnt += next_stage_check(p_tetrimino);
+		line_cnt += next_stage_check(p_tetrimino, &score);
+
+		//line_cnt를 확인해서 레벨 변경
+		if (line_cnt == 2) {
+			line_cnt = 0;
+			level++;
+			time_interval = time_interval / 2;
+			time_interval_update = time_interval;
+		}
 
 		//블록 및 게임판 재출력
-		Move_Cursor(10, 30);
+		Move_Cursor(30, 6);
 		printf("cleard line : %d", line_cnt);
+		Move_Cursor(30, 4);
+		printf("score : %d", score);
+		Move_Cursor(30, 2);
+		printf("Level : %d", level);
 		print_tetrimino(p_tetrimino);
 		Move_Cursor(0, 0);
 		print_board();
@@ -437,8 +427,7 @@ void make_new_tetrimino(block * p_tetrimino) {
 
 //tetrimino의 정보를 설정하는 함수
 void set_tetrimino_value(block * p_tetrimino) {
-	//int t_type = rand() % 7;
-	int t_type = 1;
+	int t_type = rand() % 7;
 	p_tetrimino->type = t_type;
 	p_tetrimino->rotate = 0;
 	p_tetrimino->pos.x = 13;
@@ -525,27 +514,33 @@ void print_blank(block * p_tetrimino) {
 }
 
 //내려오는 tetrimino 주위에 다른 장애물이 있는지 판별하는 함수
-void check(block * p_tetrimino, int x, int y) {
-	int dx[4] = { 1, 0, -1, 0 };
-	int dy[4] = { 0, 1, 0, -1 };
-	int condition;
-	int can_go;
-	visit[x][y] = 1;
+int check(block * p_tetrimino, int rotate, int x, int y) {
+	x += 2;
+	y += 2;
 
-	int i;
-	for (i = 0; i < 4; i++) {
-		condition = (x < 1 || y > 19 || x > 20 || visit[x + dx[i]][y + dy[i]]);
-		can_go = !(board[x + dx[i]][y + dy[i]] == 1 || board[x + dx[i]][y + dy[i]] == 2);
-		if (condition == 0 && can_go == 0)
-			p_tetrimino->near_block[i] = 1;
-		if (condition == 0 && board[x + dx[i]][y + dy[i]] == 10)
-			check(p_tetrimino, x + dx[i], y + dy[i]);
+	for (int dX = -2; dX <= 2; ++dX)
+	{
+		for (int dY = -2; dY <= 2; ++dY)
+		{
+			if ((x + dX >= 0) && (x + dX < 22) && (y + dY < 20))
+			{
+				if ((board[x + dX][y + dY] == 1 || board[x + dX][y + dY] == 2) &&
+					tetriminos[p_tetrimino->type][rotate][2 + dY][2 + dX] == 1)
+					return 0;
+			}
 
+			else if (tetriminos[p_tetrimino->type][rotate][2 + dY][2 + dX] != 0)
+			{
+				return 0;
+			}
+		}
 	}
+
+	return 1;
 }
 
 //한줄을 다 채웠을때 줄 수를 세고 화면을 조정하는 함수
-int next_stage_check(block* p_tetrimino) {
+int next_stage_check(block * p_tetrimino, int* score) {
 	int i, j;
 	int next_stage_count = 0;
 	int cnt = 0;
@@ -557,6 +552,7 @@ int next_stage_check(block* p_tetrimino) {
 		}
 		if (cnt == 20) {
 			next_stage_count++;
+			*score += 100 * next_stage_count;
 			down_board(p_tetrimino, i);
 			system("cls");
 		}
@@ -566,7 +562,7 @@ int next_stage_check(block* p_tetrimino) {
 }
 
 //한 줄을 다 채웠을 때 그 윗 줄을 하나 내리는 함수
-void down_board(block* p_tetrimino, int line) {
+void down_board(block * p_tetrimino, int line) {
 	int i, j;
 	for (i = line; i > 1; i--) {
 		for (j = 1; j < 21; j++) {
